77d7c3a601cf6c0c4f9ecafbc9628defd050ec9a
noah978 <noahkeck@mindspring.com>
2022-06-07 21:41:50 -0500

    Alter PDF printing method

--- _includes/pdf.js
+++ _includes/pdf.js
@@ -7,8 +7,27 @@ var specialElementHandlers = {
     }
 };
 
+export default function printDiv({divId, title}) {
+    let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+
+    mywindow.document.write(`<html><head><title>${title}</title><style>`);
+    mywindow.document.write(document.getElementsByTagName('style')[0].innerHTML);
+    mywindow.document.write('</style></head><body >');
+    mywindow.document.write(document.getElementById(divId).innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+
+    return true;
+};
+
 $('#cmd').click(function () {
-    doc.html(document.getElementById('content'), {
+    printDiv({divId: 'content', title: 'noahkeck'});
+    /*doc.html(document.getElementById('content'), {
         callback: function (doc) {
             doc.save('noahkeck.pdf');
           },
@@ -16,5 +35,5 @@ $('#cmd').click(function () {
             scale: 0.22
         },
         margin: [15, 15, 15, 15]
-    });
+    });*/
 });
